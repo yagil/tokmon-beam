41f5876998020d3d41c7b25ebf2902d38c555c61
Yagil Burowski <yagil@elementlabs.ai>
2023-04-25 14:26:52 -0400

    update history rows in real time

--- nextjs-app/pages/index.tsx
+++ nextjs-app/pages/index.tsx
@@ -21,12 +21,27 @@ export type IndexProps = {
 export default function Index({ wssPort } : IndexProps) {
   const [summaries, setSummaries] = useState<TokenUsageSummary[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const [lastUpdatedId, setLastUpdatedId] = useState<string | null>(null);
 
   const router = useRouter();
+
+  const updateSummaries = (data: any) => {
+    setSummaries((prevSummaries) => {
+      const newSummaries = [...prevSummaries];
+      const index = prevSummaries.findIndex((summary) => summary.tokmon_conversation_id === data.tokmon_conversation_id);
+      if (index !== -1) {
+        newSummaries[index] = data;
+      } else {
+        return [data, ...prevSummaries];
+      }
+      return newSummaries;
+    });
+  }
+    
   useEffect(() => {
     fetch('/api/summary')
       .then((res) => res.json())
-      .then((data) => setSummaries(data))
+      .then((data) => { setSummaries(data); })
       .catch((error) => {
         console.error('Error fetching summaries:', error)
         setError(error.message);
@@ -43,13 +58,26 @@ export default function Index({ wssPort } : IndexProps) {
       const type = message.type;
 
       if (type === 'tokenUsageSummary') {
-        setSummaries((prevSummaries) => [data, ...prevSummaries]);
+        updateSummaries(data);
+        setLastUpdatedId(data.tokmon_conversation_id);
       }
     }
     return () => {
       ws.close();
     }
   }, []);
+
+  useEffect(() => {
+    if (!lastUpdatedId) return;
+    const summaryRow = document.getElementById(`summary-${lastUpdatedId}`);
+    if (summaryRow) {
+      summaryRow.classList.add('bg-slate-200');
+      setTimeout(() => {
+        summaryRow.classList.remove('bg-slate-200');
+        setLastUpdatedId(null);
+      }, 1000);
+    }
+  }, [summaries]);
 
   const deleteSummary = async (id: string) => {
     try {
@@ -67,22 +95,28 @@ export default function Index({ wssPort } : IndexProps) {
 
   if (error) return <div className="container mx-auto p-10">Error: {error}</div>;
 
+  const totalTokens = (summary: TokenUsageSummary) => {
+    const { total_tokens } = summary.total_usage as { total_tokens: number };
+    return total_tokens;
+  };
+
   return (
     <div className="container mx-auto p-10">
-      <h1 className="text-2xl font-bold mb-4">Conversations</h1>
+      <h1 className="text-2xl font-bold mb-10">Conversations History</h1>
       <table className="w-full border-collapse">
         <thead>
-          <tr>
-            <th className="border p-2">Timestamp</th>
-            <th className="border p-2">Program</th>
+          <tr className="bg-gray-100 text-left">
+            <th className="border p-2">Last updated</th>
+            <th className="border p-2">Program invocation</th>
             <th className="border p-2">Conversation ID</th>
-            <th className="border p-2">Total Cost</th>
+            <th className="border p-2">Total tokens</th>
+            <th className="border p-2">Total cost</th>
             <th className="border p-2">Actions</th>
           </tr>
         </thead>
         <tbody>
           {summaries.map((summary) => (
-            <tr key={summary.id}>
+            <tr key={summary.id} id={`summary-${summary.tokmon_conversation_id}`}>
               <td className="border p-2">{formatDate(summary.timestamp)}</td>
               <td className="border p-2">{summary.monitored_program}</td>
               <td
@@ -91,6 +125,7 @@ export default function Index({ wssPort } : IndexProps) {
               >
                 {summary.tokmon_conversation_id}
               </td>
+              <td className="border p-2">{totalTokens(summary)}</td>
               <td className="border p-2">${summary.total_cost}</td>
               <td className="border p-2">
                 <button
